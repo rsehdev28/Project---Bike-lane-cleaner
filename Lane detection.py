a13e06a77d6a8abb6bbae4087a2b33ee3fba8f79
Kaustubh Upadhyay <kaustubhupadhyay2008@gmail.com>
2024-02-05 16:06:30 -0700

    Changing the approach

--- Lane detection.py
+++ Lane detection.py
@@ -1,7 +1,7 @@
 import cv2 
 import numpy as np
 
-image = cv2.imread('test_image1.jpg') # Reads the image in the folder and returns it as multidimensional arrays containing intensities of each pixel. Try playing with by changing images.
+image = cv2.imread('test_image4.jpg') # Reads the image in the folder and returns it as multidimensional arrays containing intensities of each pixel. Try playing with by changing images.
 lane_image = np.copy(image) #copies the image array into another variable for greyscaling
 gray = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
 #cv2.imshow('result',gray) # renders the image and shows it wit a window name
