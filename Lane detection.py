5b2a7e98f1d576ead5a1d6c7f5b2cc21872b00b0
Kaustubh Upadhyay <kaustubhupadhyay2008@gmail.com>
2024-03-11 12:53:20 -0700

    Basic Working Algorithm

--- Lane detection.py
+++ Lane detection.py
@@ -8,6 +8,41 @@ def canny(image):
     canny = cv2.Canny(blur,50,150) # Detection of Sharp edges/ strong gradient of pixels in the image
     return canny
 
+def make_coordinates(image, line_parameters):
+    slope,intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+
+def display_lines(image,lines):
+    line_image = np.zeros_like(image) #a zero value array containing values similar to the given image (all black)
+    if lines is not None:
+        for x1,y1,x2,y2 in lines:
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+            #All the lines that we detected in our graident image, we just drew them over a black image(line_image) with the same dimensions as out actual image
+    return line_image
+
+def average_slope_intercept(image,lines):
+    left_fit =[]
+    right_fit =[]
+    for line in lines:
+        x1,y1,x2,y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2),(y1,y2),1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope<0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+    left_fit_average = np.average(left_fit,axis =0)
+    right_fit_average =np.average(right_fit,axis=0)
+    left_line = make_coordinates(image,left_fit_average)
+    right_line = make_coordinates(image,right_fit_average)
+    return np.array([left_line,right_line])
+
+
 def region_of_interest(image):  # Identifies the region of interest by isolating the edge lines based on coordinates and using a mask
     height = image.shape[0]
     polygons = np.array([[(475,height),(600,height),(500,75)]])
@@ -15,14 +50,23 @@ def region_of_interest(image):  # Identifies the region of interest by isolating
     cv2.fillPoly(mask,polygons,255)
     masked_image = cv2.bitwise_and(image,mask)
     return masked_image
+
 image = cv2.imread('test_image4.jpg') # Reads the image in the folder and returns it as multidimensional arrays containing intensities of each pixel. Try playing with by changing images.
 lane_image = np.copy(image) #copies the image array into another variable for greyscaling
-canny = canny(lane_image)
+canny_image = canny(lane_image)
+
 #cv2.imshow("Result",canny) renders the image and shows it with a window name
 #cv2.waitKey(0)  Displays the image
 #plt.imshow(canny) Displays the image with a graph having x and y axes 
 #plt.show() 
-cropped_image = region_of_interest(canny)
-lines = cv2.HoughLinesP(cropped_image,2,np.pi/180) # To find the lines that best define our edges
-cv2.imshow("result",cropped_image)
+
+cropped_image = region_of_interest(canny_image)
+lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5) # To find the lines that best define our edges
+averaged_lines = average_slope_intercept(lane_image,lines)
+line_image = display_lines(lane_image,averaged_lines)
+combo_image = cv2.addWeighted(lane_image,0.6,line_image,2,2)
+
+#cv2.imshow("result",line_image)
+#cv2.waitKey(0)
+cv2.imshow("result",combo_image)
 cv2.waitKey(0)
